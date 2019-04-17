daaf4b42512a2fe936312a175fdf6b6cab2dc5ad
Ruairidh Holwill <roore@hotmail.co.uk>
2019-04-17 14:59:23 +0100

    save, all and delete_all functions

---
+++ db/console.rb
@@ -0,0 +1,15 @@
+require("pry")
+require_relative("../models/album")
+require_relative("../models/artist")
+
+Album.delete_all()
+Artist.delete_all()
+
+artist1 = Artist.new({'name' => 'Katy Perry'})
+artist1.save()
+
+album1 = Album.new({'title' => 'Witness', 'genre' => 'Pop', 'artist_id' => artist1.id})
+album1.save()
+
+binding.pry
+nil
---
+++ models/album.rb
@@ -0,0 +1,48 @@
+require("pg")
+require_relative("artist")
+require_relative("../db/sql_runner")
+
+class Album
+
+  attr_reader :title, :genre, :artist_id, :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO albums
+    (
+      title,
+      genre,
+      artist_id
+    )
+      VALUES
+    (
+      $1, $2, $3
+    )
+      RETURNING id;"
+
+    values = [@title, @genre, @artist_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM albums;"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM albums;"
+    albums = SqlRunner.run(sql)
+    return albums.map { |album| Album.new(album) }
+  end
+
+
+
+
+end
